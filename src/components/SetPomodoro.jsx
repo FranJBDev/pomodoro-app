eb1da51cb7f3881ad33c700a1f2be465d376f1de
Gandhyman <gandhycoder@gmail.com>
2021-11-15 22:20:01 -0700

    Arreglado estilo de inputs de configuracion

--- src/components/SetPomodoro.jsx
+++ src/components/SetPomodoro.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react'
 import { SettingsContext } from '../context/SettingsContext'
-import Button from './Button'
 
 const SetPomodoro = () => {
     const [newTimer, setNewTimer] = useState({
